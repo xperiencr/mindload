8ac7aa10a93e3a02d8ec1df7ed1509061557f779
Diego Jardon <diegojardonc@gmail.com>
2021-04-12 11:19:40 -0500

    Add: scaffold project layout

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import HelpSection from "./components/HelpSection";
+
 import './index.css';
 
 function Popup() {
-  return null;
+  return (
+    <div className="Popup">
+      <HelpSection isOpen />
+    </div>
+  );
 }
 
 ReactDOM.render(<Popup />, document.getElementById('root'));

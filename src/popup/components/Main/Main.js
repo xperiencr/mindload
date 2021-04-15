ddd6fa6df94fb9c868805d5a7909d0bfeeb1a30f
Diego Jardon <diegojardonc@gmail.com>
2021-04-15 08:53:41 -0500

    Add: Main and Navigation component base structure

---
+++ src/popup/components/Main/Main.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import sendIcon from './assets/send.svg';
+
+function Main({ onCreateNote }) {
+  const [noteContent, setNoteContent] = useState('');
+
+  return (
+    <div className="Main">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          onCreateNote({ content: noteContent });
+        }}
+        className="Main__form"
+      >
+        <input
+          className="Main__input"
+          type="text"
+          placeholder="Write it down"
+          onChange={(event) => setNoteContent(event.target.value)}
+          value={noteContent}
+        />
+        <button className="Main__submit" type="submit">
+          <img src={sendIcon} alt="Send" />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+Main.propTypes = {
+  onCreateNote: PropTypes.func.isRequired,
+};
+
+export default Main;
--- src/popup/components/index.js
+++ src/popup/components/index.js
@@ -1,1 +1,2 @@
-export { default } from './Main';
+export { default as Main } from './Main';
+export { default as Navigation } from './Navigation';

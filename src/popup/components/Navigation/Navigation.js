996e632a01382532f377acceef3b078e2835e20a
Diego Jardon <diegojardonc@gmail.com>
2021-04-20 15:45:01 -0500

    Add: Navigation styles

--- src/popup/components/Navigation/Navigation.js
+++ src/popup/components/Navigation/Navigation.js
@@ -11,13 +11,13 @@ import helpIcon from './assets/help.svg';
 function Navigation({ className, goHome, openHelp }) {
   return (
     <nav className={`${className} Navigation`}>
-      <button onClick={goHome} type="button" className="Navigation__home">
+      <button onClick={goHome} type="button" className="Navigation__link Navigation__home">
         <img src={homeIcon} alt="Home" />
       </button>
-      <button onClick={() => {}} type="button" className="Navigation__expand-queue">
+      <button onClick={() => {}} type="button" className="Navigation__link Navigation__expand-queue">
         <img src={expandMoreIcon || expandLessIcon} alt="Expand" />
       </button>
-      <button onClick={openHelp} type="button" className="Navigation__help">
+      <button onClick={openHelp} type="button" className="Navigation__link Navigation__help">
         <img src={helpIcon} alt="Help" />
       </button>
     </nav>

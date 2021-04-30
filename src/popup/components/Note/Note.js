e1a5a7edfdd7e2acf8d7ec728d9dec59e193fe54
Alex Fernandez <41647351+A1llex@users.noreply.github.com>
2021-04-30 16:20:25 -0500

    arreglos añadidos

--- src/popup/components/Note/Note.js
+++ src/popup/components/Note/Note.js
@@ -1,41 +1,27 @@
-import React, { useState } from 'react';
-import './Note.css';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import "./Note.css";
+import PropTypes from "prop-types";
 import closeIcon from "./close.svg";
 
-
-function Note({content,isUrgent,onRemove}) {
-  const [isHidden,setHidden] = useState(true)
+function Note({ content, isUrgent, onRemove }) {
+  const [isHidden, setHidden] = useState(true);
 
   const botonDeCerrar = (
     <button>
       <img src={closeIcon} />
     </button>
   );
-  
-  if (isUrgent){
-    return (
-      <div 
-        className="Note-urgent" 
-        onMouseEnter={()=>setHidden(false)}
-        onMouseLeave={()=>setHidden(true)}
-      >
-          <p>{content}</p>
-          {!isHidden && botonDeCerrar}
-      </div>
-    );
-  }else{
-    return (
-      <div 
-        className="Note" 
-        onMouseEnter={()=>setHidden(false)}
-        onMouseLeave={()=>setHidden(true)}
-      >
-          <p>{content}</p>
-          {!isHidden && botonDeCerrar}
-      </div>
-    );
-  }
+
+  return (
+    <div
+      className={`Note ${isUrgent ? "Note--urgent" : ""}`}
+      onMouseEnter={() => setHidden(false)}
+      onMouseLeave={() => setHidden(true)}
+    >
+      <p>{content}</p>
+      {!isHidden && botonDeCerrar}
+    </div>
+  );
 }
 
 Note.propTypes = {

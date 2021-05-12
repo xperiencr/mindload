80965d4e4d501a1a59c167bf7ea63d32e554f33e
bcalvario <bcalvario@ciencias.unam.mx>
2021-05-12 09:06:57 -0500

    Fix: restore Popup.js

--- src/popup/Popup.js
+++ src/popup/Popup.js
@@ -1,45 +1,21 @@
 import React from 'react';
 
-import { Main, Navigation, Queue } from './components';
+import { Main, Navigation } from './components';
 
 import './index.css';
 
 export default function Popup() {
-  const noteList = [
-    {
-      id: 'abcdef',
-      content: 'Lorem ipsum dolor sit amet',
-      isUrgent: false,
-    },
-    {
-      id: 'ghijkl',
-      content: 'Lorem ipsum dolor sit amet',
-      isUrgent: true,
-    },
-    {
-      id: 'ghijklasa',
-      content: 'Lorem ipsum dolor sit amet',
-      isUrgent: false,
-    },
-    {
-      id: 'ghijksdfgl',
-      content: 'Lorem ipsum dolor sit amet',
-      isUrgent: true,
-    },
-    {
-      id: 'ghijkwedgfdwdl',
-      content: 'Lorem ipsum dolor sit amet',
-      isUrgent: false,
-    },
-  ];
-
   return (
     <div className="Popup">
-      <Queue
-        notes={noteList}
-        onAdd={(newNote) => console.log(`add: ${newNote.content.slice(0, 10)}`)}
-        onRemove={(id) => console.log(`remove: ${id}`)}
-        onClose={() => console.log('close')}
+      <Main
+        onCreateNote={(newNote) => {
+          console.log('Note to be created: ');
+          console.log(newNote);
+        }}
+      />
+      <Navigation
+        goHome={() => console.log('Went home')}
+        openHelp={() => console.log('Opened help')}
       />
     </div>
   );

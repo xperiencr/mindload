1a2cc3cada106397f1e6ff2c6626e06a15b95ba6
Diego Jardon <diegojardonc@gmail.com>
2021-05-12 10:13:51 -0500

    Add: storage

--- src/services/storage/Storage.js
+++ src/services/storage/Storage.js
@@ -1,3 +1,37 @@
 export default class Storage {
-  constructor() {}
+  static async saveQueueNote(newNote) {
+    const currentNotes = await browser.storage.local.get('queueNotes');
+    const newNotes = [...currentNotes, newNote];
+    return browser.storage.local.set('queueNotes', newNotes);
+  }
+
+  static async deleteQueueNote(noteId) {
+    const currentNotes = await browser.storage.local.get('queueNotes');
+    const newNotes = currentNotes.filter((note) => note.id !== noteId);
+    return browser.storage.local.get('queueNotes', newNotes);
+  }
+
+  static async getQueueNotes() {
+    return browser.storage.local.get('queue');
+  }
+
+  static setListener(listener) {
+    browser.storage.local.onChanged = listener;
+  }
+
+  static async saveArchiveNote(newNote) {
+    const currentNotes = await browser.storage.local.get('archiveNotes');
+    const newNotes = [...currentNotes, newNote];
+    return browser.storage.local.set('archiveNotes', newNotes);
+  }
+
+  static async deleteArchiveNote(noteId) {
+    const currentNotes = await browser.storage.local.get('archiveNotes');
+    const newNotes = currentNotes.filter((note) => note.id !== noteId);
+    return browser.storage.local.set('archiveNotes', newNotes);
+  }
+
+  static async getArchiveNotes() {
+    return browser.storage.local.get('archiveNotes');
+  }
 }

cd909fc3b7ed5f7b65a69e7b08b8c99cc87591fe
Diego Jardon <diegojardonc@gmail.com>
2021-05-22 16:44:28 -0500

    Add: render provider archive

--- src/archive/index.js
+++ src/archive/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { State } from 'services';
+
 import './index.css';
 
 function Archive() {
   return null;
 }
 
-ReactDOM.render(<Archive />, document.getElementById('root'));
+ReactDOM.render(
+  <State.Archive.Provider>
+    <Archive />
+  </State.Archive.Provider>,
+  document.getElementById('root'),
+);
